98f39e455121829e815e4ae73685717280c2cb37
Nomas Tickles <nomastickles@users.noreply.github.com>
2023-01-09 00:16:01 -0800

    redux dev tools for frontend

--- frontend/src/context.tsx
+++ frontend/src/context.tsx
@@ -14,8 +14,28 @@ interface Props {
 
 const ContextProvider = ({ children }: Props): React.ReactElement => {
   const [state, dispatch] = React.useReducer(AppSlice.reducer, initialState);
+  const devToolsConnectionRef = React.useRef({} as any);
+  const lastDispatch = React.useRef({} as any);
+
+  const wrappedDispatch = React.useCallback((stuff: any) => {
+    lastDispatch.current = stuff;
+    dispatch(stuff);
+  }, []);
+
+  React.useEffect(() => {
+    devToolsConnectionRef.current = (
+      window as any
+    ).__REDUX_DEVTOOLS_EXTENSION__?.connect({
+      name: "gunshots",
+    });
+  }, []);
+
+  React.useEffect(() => {
+    devToolsConnectionRef.current?.send(lastDispatch?.current || "init", state);
+  }, [state]);
+
   return (
-    <DispatchContext.Provider value={dispatch}>
+    <DispatchContext.Provider value={wrappedDispatch}>
       <StateContext.Provider value={state}>{children}</StateContext.Provider>
     </DispatchContext.Provider>
   );
